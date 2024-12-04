10f6ed255ee8981d62d03e9c9539a5fe563ee319
jaison-silva <jaisonjoy3032gmail.com>
2024-12-04 11:29:37 +0530

    swiggy

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-// const React = require("react");
-// const ReactDom = require("react-dom");
+const React = require("react");
+const ReactDom = require("react-dom/client");
 
 
-const heading = React.createElement("h1", {}, "Namasthe React!");
+// const heading = React.createElement("h1", {}, "Namasthe React!");
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDom.createRoot(document.getElementById("root"));
 
-console.log(heading); 
-root.render(heading); 
+// console.log(heading);  
+// root.render(heading); 
+
+
+// component Composition
+
+let Heading = () => (
+    <h1>Hello world</h1>
+)
+
+let Para = () => (
+    <div>
+        <Heading />
+    <p>Namasthe js</p>
+    </div>
+)
+
+root.render(<Para/>)
