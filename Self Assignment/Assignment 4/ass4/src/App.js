e8f59ce97a705c4e2d0430a9a610b078ef8e799c
jaison-silva <jaisonjoy3032gmail.com>
2025-12-05 18:09:02 +0530

    Root: Finalized Week 18 assignments, merged .gitignore files, and updated sub-projects.

--- Self Assignment/Assignment 4/ass4/src/App.js
+++ Self Assignment/Assignment 4/ass4/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { useRef } from "react";
+import { useRef } from "react";
 
 function App() {
+
+  const inputval = useRef(null)
+
+  function refHook (){
+      console.log(inputval.current.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Enter your name</h3>
+      <input ref={inputval} />
+      <button type="submit" onClick={refHook}>Submit</button>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+// import React, { useState } from 'react';
+
+// function ControlledInput() {
+//   const [inputValue, setInputValue] = useState('');
+
+//   const handleChange = (event) => {
+//     setInputValue(event.target.value);  // Update state with input value
+//   };
+
+//   const handleSubmit = () => {
+//     alert('Entered Value: ' + inputValue);  // Access value from state
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={inputValue}  // The value is controlled by React state
+//         onChange={handleChange}  // Handle changes to input
+//       />
+//       <button onClick={handleSubmit}>Submit</button>
+//     </div>
+//   );
+// }
+
+// export default ControlledInput;
